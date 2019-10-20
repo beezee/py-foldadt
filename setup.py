7223867408d145dc1d671511f3e7113c7d3115b4
beezee <brian.zeligson@gmail.com>
2019-10-20 17:09:38 -0400

    try this instead

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="py-foldadt",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     author="beezee",
     description="Zero dependency, mypy friendly algebraic data types",
